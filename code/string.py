ffea2546ba713bdafee0942f221be0d7eb861d18
Luis Castillo <xt2481@rimac.com.pe>
2022-10-20 17:20:31 -0500

    se agrega variables

--- code/string.py
+++ code/string.py
@@ -1,7 +1,12 @@
 #primer programa de string
 # variables     
-_nombre ="Lucas"
-_apellido ="Vega"
-_edad =2
-_nombres_completo = _nombre + _apellido  
-print("Mi nombre es :" + _nombres_completo + "tengo :" + str(_edad) + " años")
+_nombre =  input("Ingrese su nombre :")
+_apellido_paterno =  input("Ingrese su apellido paterno :")
+_apellido_materno =  input("Ingrese su apellido materno :")
+#_apellido ="Vega"
+#_edad =2
+#_nombres_completo = _nombre + _apellido  
+#print("Mi nombre es :" + _nombres_completo + "tengo :" + str(_edad) + " años")
+_nombre_completo = _nombre + " " + _apellido_paterno + " " + _apellido_materno
+print("Hola estiamdo "+ _nombre_completo +  " Bienvenido al curso de")
+
